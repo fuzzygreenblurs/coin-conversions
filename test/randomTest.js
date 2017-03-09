599953a3803c17b48c22670d9bd8817afc64701a
Akhil Sankar <akhil.sankar@welltok.com>
2017-03-09 14:18:30 -0700

    integrated mocha with es6

--- test/randomTest.js
+++ test/randomTest.js
@@ -1,4 +1,4 @@
-var assert = require('assert')
+import assert from 'assert'
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
